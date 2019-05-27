8cd05b8c96901de13e9a9d535ec400b1154fa39b
kimchheng_kao_kh <kimchheng.kao@ascendcorp.com>
2019-05-27 15:19:38 +0700

    init

---
+++ src/main/resources/archetype-resources/src/main/java/__classNamePrefix__Application.java
@@ -0,0 +1,13 @@
+package ${package};
+ 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+ 
+@SpringBootApplication
+public class ${classNamePrefix}Application {
+ 
+    public static void main(String[] args) {
+        SpringApplication.run(${classNamePrefix}Application.class, args);
+    }
+}
+
