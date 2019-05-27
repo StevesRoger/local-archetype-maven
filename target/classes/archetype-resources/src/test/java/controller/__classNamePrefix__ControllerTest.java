174b699d382d84d9de5b8c8190d0103bfcb1a77d
kimchheng_kao_kh <kimchheng.kao@ascendcorp.com>
2019-05-27 16:24:44 +0700

    update

---
+++ target/classes/archetype-resources/src/test/java/controller/__classNamePrefix__ControllerTest.java
@@ -0,0 +1,16 @@
+package ${package}.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class ${classNamePrefix}ControllerTest {
+
+    @Test
+    public void test() {
+     
+    }
+
+}
